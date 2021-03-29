f9849676ab7e770ca5fd3cafd27ecf8256b34d31
micat1996 <34137082+micat1996@users.noreply.github.com>
2021-03-29 21:39:28 +0900

    Add Character Mesh

---
+++ Assets/Scripts/Components/AnimController/PlayerCharacterAnimController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 플레이어 캐릭터에 사용되는 Animator 컴포넌트를 제어하기 위한 컴포넌트입니다.
+public sealed class PlayerCharacterAnimController : AnimController
+{
+	private PlayerableCharacter _PlayerableCharacter;
+
+	private void Awake()
+	{
+		_PlayerableCharacter = GetComponent<PlayerableCharacter>();
+	}
+
+	private void Update()
+	{
+		SetParam("_VelocityLength", _PlayerableCharacter.movement.velocity.magnitude);
+		SetParam("_IsInAir", !_PlayerableCharacter.movement.isGrounded);
+	}
+
+
+}
