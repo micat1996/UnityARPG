abb155cb8250cb92d30a45a14b8127f9278a5914
micat1996 <34137082+micat1996@users.noreply.github.com>
2021-03-30 05:03:04 +0900

    ClosableWndTest

---
+++ Assets/Scripts/Components/ScreenInstance/GameScreenInstance.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScreenInstance : ScreenInstance
+{
+
+
+
+	protected override void Awake()
+	{
+		base.Awake();
+
+		var testWnd = ResourceManager.Instance.LoadResource<GameObject>(
+			"Wnd_Test",
+			"Prefabs/Wnds/Wnd_Test").GetComponent<TestWnd>();
+
+		CreateWnd(testWnd);
+	}
+}
