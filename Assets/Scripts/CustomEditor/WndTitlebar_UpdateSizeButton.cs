5fc99f004f619cee4cfd837812736bf1099364b3
micat1996 <34137082+micat1996@users.noreply.github.com>
2021-03-30 21:44:49 +0900

    Add Closable Wnd Form

---
+++ Assets/Scripts/CustomEditor/WndTitlebar_UpdateSizeButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ClosableWndTitlebar), true)]
+public sealed class WndTitlebar_UpdateSizeButton:Editor
+{
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+
+		// 재정의할 컴포넌트 인스턴스를 얻습니다.
+		ClosableWndTitlebar component = target as ClosableWndTitlebar;
+
+		// 버튼 추가
+		if (GUILayout.Button("UpdateSize"))
+			AttachToGround(component);
+	}
+
+	private void AttachToGround(ClosableWndTitlebar component) =>
+		component.UpdateSize();
+}
